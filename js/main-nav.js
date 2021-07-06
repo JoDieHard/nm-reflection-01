08c1bd484640709b11005842e3b04f8ced3fb9b3
JoDieHard <josephbowmandesigns@gmail.com>
2021-07-06 19:37:59 +0100

    Scroll Handler
    This commit adds a scroll handler to tell when the user is scrolling up/down the page and opens/closes the sliding header.

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,14 +1,43 @@
 
 const header = $('header');
 const nav = $('.main-nav');
-const slidingNav = $('.slidingNav');
 const createDiv = function () {  $('.homepage').prepend($('<div>', {"class": "slidingNav"})); }; // This Function creates an empty Div
+let lastScrollValue = 0;
 
-createDiv();
-setTimeout(function(){ 
-    $('header').clone().appendTo('.slidingNav');
-    $('.main-nav').clone().appendTo('.slidingNav');
-    console.log('cloned the shits');
- }, 1000);
+$( window ).scroll( function(event) {
+    // console.log('You are scrolling!');
+    let sv = $(this).scrollTop();       // This gets the Current Scroll Value
+
+    if ( sv > lastScrollValue && sv > 220 ) {  // Check if lastScrollValue is less than current scroll value (sv)
+        setTimeout(function(){ 
+        $('.slidingNav').css('transform', 'translateY(-210px)');
+        }, 1500);
+
+        if ( $('.slidingNav').length === 0) {   // Check there is no navSliders befor making another
+            console.log('There is no slidingNav!');
+            createDiv();
+            setTimeout(function(){ 
+                $('header').clone().appendTo('.slidingNav');
+                $('.main-nav').clone().appendTo('.slidingNav');
+                console.log('cloned the shits');
+            }, 1000);
+        }
+        // console.log('scrolling down...');
+    } else {
+        // console.log('scrolling up!!!');
+        setTimeout(function(){ 
+        $('.slidingNav').css('transform', 'translateY(0px)');
+        }, 1500);
+
+    };
+
+    if ( sv === 0 ) {
+        $('.slidingNav').remove();
+    };
+    lastScrollValue = sv;
+});
+
+
+
 
 
