54b07dd8c9f558a860d272b4a1fe54dc71c668be
JoDieHard <josephbowmandesigns@gmail.com>
2021-07-07 12:12:55 +0100

    Sliding Nav Fixes
    This commit creates the scrolling animation smoother.
    
    Known issue: slidingNav disappears under website when sidebar is opened.

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,7 +1,7 @@
 
 const header = $('header');
 const nav = $('.main-nav');
-const createDiv = function () {  $('.homepage').prepend($('<div>', {"class": "slidingNav"})); }; // This Function creates an empty Div
+const createDiv = function () {  $('.website').prepend($('<div>', {"class": "slidingNav"})); }; // This Function creates an empty Div
 let lastScrollValue = 0;
 
 $( window ).scroll( function(event) {
@@ -10,8 +10,8 @@ $( window ).scroll( function(event) {
 
     if ( sv > lastScrollValue && sv > 220 ) {  // Check if lastScrollValue is less than current scroll value (sv)
         setTimeout(function(){ 
-        $('.slidingNav').css('transform', 'translateY(-210px)');
-        }, 1500);
+        $('.slidingNav').css('transform', 'translateY(0px)');
+        }, 500);
 
         if ( $('.slidingNav').length === 0) {   // Check there is no navSliders befor making another
             console.log('There is no slidingNav!');
@@ -26,8 +26,8 @@ $( window ).scroll( function(event) {
     } else {
         // console.log('scrolling up!!!');
         setTimeout(function(){ 
-        $('.slidingNav').css('transform', 'translateY(0px)');
-        }, 1500);
+        $('.slidingNav').css('transform', 'translateY(210px)');
+        }, 500);
 
     };
 
